568091ea10938c98cb37bddd75027471180cca4e
abhinav <abhinavsharma24999@gmail.com>
2020-03-12 00:02:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/passcodeview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.passcodeview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.hanks.passcodeview.PasscodeView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        passcodeView.setPasscodeLength(5).setLocalPasscode("00000").listener = object :
+            PasscodeView.PasscodeViewListener {
+            override fun onSuccess(number: String?) {
+                startActivity(Intent(this@MainActivity,WebViewActivity::class.java))
+            }
+
+            override fun onFail() {
+                Toast.makeText(applicationContext,"Pass-code is Wrong.",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
