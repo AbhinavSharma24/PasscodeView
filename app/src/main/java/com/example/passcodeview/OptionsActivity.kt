164ec196dd9fbdf20471dede544c53fcd6cb6a48
abhinav <abhinavsharma24999@gmail.com>
2020-03-18 20:54:12 +0530

    Initial Commit

--- app/src/main/java/com/example/passcodeview/OptionsActivity.kt
+++ app/src/main/java/com/example/passcodeview/OptionsActivity.kt
@@ -1,27 +1,57 @@
 package com.example.passcodeview
 
 import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_options.*
+
+
 
 class OptionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE)
+
         setContentView(R.layout.activity_options)
 
         first.setOnClickListener {
-            startActivity(Intent(this,WebViewActivity::class.java))
+            val startWebViewIntent = Intent(this, WebViewActivity::class.java)
+            startWebViewIntent.putExtra("website", "1")
+            startActivity(startWebViewIntent)
         }
 
         second.setOnClickListener {
-            startActivity(Intent(this,WebViewActivity::class.java))
+            val startWebViewIntent = Intent(this, WebViewActivity::class.java)
+            startWebViewIntent.putExtra("website", "2")
+            startActivity(startWebViewIntent)
         }
 
         third.setOnClickListener {
-            Toast.makeText(this, "FUCK OFF", Toast.LENGTH_SHORT).show()
+            val startWebViewIntent = Intent(this, WebViewActivity::class.java)
+            startWebViewIntent.putExtra("website", "3")
+            startActivity(startWebViewIntent)
         }
+
+        /*vpn.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse("https://play.google.com/store/apps/details?id=org.hola&hl=en_IN")
+            startActivity(i)
+        }*/
+    }
+
+    override fun onPause() {
+        window.decorView.rootView.setBackgroundColor(Color.BLUE)
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        startActivity(Intent(applicationContext,MainActivity::class.java))
+        super.onRestart()
     }
 }
